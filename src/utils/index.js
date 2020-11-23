2a8a37bf5a92bb031006518b6a0bfa87f796a09b
Alexander Rodriguez <ing.alexrcali@gmail.com>
2020-11-22 19:01:13 -0500

    fix bug

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,12 @@
 const validData = (data, required = false) => {
   let control = true
-  
   if (data) {
     control = (control && data.identification < 9999999999 && data.identification > 0)
     control = (control && data.fullname !== '')
+    control = (control && String(data.type_identification).length === 2)
   } else {
     control = required ? false : true
   }
-  
-  
   return control
 }
 
--- test/validations.spec.js
+++ test/validations.spec.js
@@ -5,9 +5,10 @@ describe('prueba de funciones de validacion', () => {
   it('validar objeto para insertar a la base de datos', () => {
     expect(validData()).toBe(true); //envio parametros y debe retorna true
     expect(validData({ nombre: 'Alexander' })).toBe(false); // envio un objeto incompleto y debe retornar  false
-    expect(validData({ fullname: 'Alexander', identification: 100 })).toBe(true); // envio un objeto incompleto y debe retornar  false
+    expect(validData({ fullname: 'Alexander', identification: 100, type_identification: 'CC' })).toBe(true); // envio datos para registro debe retornar true
     expect(validData({ fullname: 'Alexander', identification: 9999999999 })).toBe(false); // el numero de identificacion supera la logintud de 10
     expect(validData({ fullname: 'Alexander', identification: -1 })).toBe(false); // el numero de identificacion enviado negativo
+    expect(validData({ fullname: 'Alexander', identification: 10000, type_identification: 'C' })).toBe(false); // envio type_identification errado debe returnar false
     expect(validData(null, true)).toBe(false); // Enviado sin data y valor required debe return false
   })
 })
